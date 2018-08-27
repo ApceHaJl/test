7233bf425d25b10be431a4aeba9bf21c7094d787
Dorosh <danya.doroshenko.03@gmail.com>
2018-08-27 16:01:52 +0300

    Мой второй комит

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -6,13 +6,26 @@ let mainList = {
 		budget: money,
 		shopname: name,
 		shopgoods: [],
-		employers: [],
+		employers: {},
 		open: false
 }
 
 alert("Ваш бюджет на сегодняшний день составляет " + money / 30 + " Грн.")
 
+
+for (let i = 0; i < 3; i++) {
+
+		let a = prompt("Какой тип товаров будем продавать?");		
+
+		if ((typeof(a)) === "string" && (typeof(a)) === null && a != "" && a.length < 50);
+		console.log("Все верно!");
+		mainList.shopgoods[i] = a;
+
+}
+
+
+
 // console.log(mainList.budget)
 // console.log(mainList.name)
-// console.log(mainList.shopgoods[1])
+console.log(mainList.shopgoods)
 // console.log(mainList.employers)
